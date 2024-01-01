2f3239bfa387d42c78c1fc1c3c54d5615e65d12f
Material-UI Tut <anigilajeridwanhorpeyemy@gmail.com>
2024-01-01 09:29:50 +0100

    Material-UI Tut

--- src/Components/MUI TUT FOLDER/db.js
+++ src/Components/MUI TUT FOLDER/db.js
@@ -1,24 +1,24 @@
 
     const notes = [
       {
-        "title": "Ridwan goes to school",
-        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
-        "category": "reminders",
-        "id": 1
+        title: "Ridwan goes to school",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
+        category: "reminders",
+        id: 1
       },
       {
-        "title": "Complete the work before going out",
-        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
+        title: "Complete the work before going out",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
         "category": "work",
-        "id": 2
+        id: 2
       },
       {
-        "title": "Order a pizza",
-        "details": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
+        title: "Order a pizza",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ea iusto, doloremque quibusdam rerum officiis quaerat perspiciatis molestiae pariatur eveniet cumque aperiam! Aspernatur ut non omnis fugit cupiditate odio doloremque?",
         "category": "todos",
-        "id": 3
+        id: 3
       }
 
-    ]
+    ];
   
   
