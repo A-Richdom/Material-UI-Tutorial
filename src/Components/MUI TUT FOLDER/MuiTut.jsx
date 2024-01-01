5be5c2cd23fb74de7b10449a97b81665fdb3fad0
Material-UI Tut <anigilajeridwanhorpeyemy@gmail.com>
2024-01-01 23:11:08 +0100

    MUI

--- src/Components/MUI TUT FOLDER/MuiTut.jsx
+++ src/Components/MUI TUT FOLDER/MuiTut.jsx
@@ -134,7 +134,7 @@ const MuiTut = () => {
             label='Title'
             name='title'
             variant='outlined'
-            // color='secondary'
+            color='secondary'
             fullWidth
             required
             error={titleError}
