14fd5d88fdd21fb5a7085da6c63c2e524ad44df4
Material-UI Tut <anigilajeridwanhorpeyemy@gmail.com>
2024-01-27 06:57:08 +0100

    Pushing to Github

--- src/Components/MUI TUT FOLDER/LayOut.jsx
+++ src/Components/MUI TUT FOLDER/LayOut.jsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
     drawerPaper: {
         width: drawerWidth,
         boxSizing: 'border-box',
-        backgroundColor: 'Pink',
+        // backgroundColor: 'Pink',
     },
     active: {
         backgroundColor: 'pink  '
